7bbdb8041ba9d863b2664141197cce760eb68715
Tam T Dang <teedang19@gmail.com>
2015-03-26 13:08:12 -0500

    empty #parse_options(argv) method for CommandLineApplication and pseudocode

--- lib/whammy/command_line_application.rb
+++ lib/whammy/command_line_application.rb
@@ -2,5 +2,9 @@ module Whammy
   class CommandLineApplication
     def initialize(argv) # argv: filenames and possible flags
     end
+
+    def parse_options(argv)
+      # use OptionParser to separate argv into filenames and flags/options to pass to a Sorter class
+    end
   end
 end
