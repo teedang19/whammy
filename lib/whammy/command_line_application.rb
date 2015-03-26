9d51dd931a5ec5bfcf38113fb7dd846de3dda7d2
Tam T Dang <teedang19@gmail.com>
2015-03-26 13:10:21 -0500

    temporary #display and #sort methods in CommandLineApplication as a reminder that this functionality is necessary, will probably be extracted somehow

--- lib/whammy/command_line_application.rb
+++ lib/whammy/command_line_application.rb
@@ -3,6 +3,12 @@ module Whammy
     def initialize(argv) # argv: filenames and possible flags
     end
 
+    def display # TODO will call a Display class ?
+    end
+
+    def sort # TODO will call a Sort class ?
+    end
+
     def parse_options(argv)
       # use OptionParser to separate argv into filenames and flags/options to pass to a Sorter class
     end
