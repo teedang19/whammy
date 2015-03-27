25d32855aa703b9e13a90ef525ad2d6cf8faaa20
Tam T Dang <teedang19@gmail.com>
2015-03-26 19:40:18 -0500

    picky difference: double quotes for strings

--- spec/whammy/command_line_application_spec.rb
+++ spec/whammy/command_line_application_spec.rb
@@ -1,8 +1,8 @@
-require_relative '../spec_helper'
+require_relative "../spec_helper"
 
 module Whammy
   describe CommandLineApplication do
-    describe '#initialize' do
+    describe "#initialize" do
     end
   end
 end
