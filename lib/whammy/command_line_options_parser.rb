6ab28aae8b4a7d61b3db882fabcc23444472c36e
Tam T Dang <teedang19@gmail.com>
2015-03-27 13:00:08 -0500

    command_line_options_parser #initialize sets configs for OptionParser instance, also stores condition in @sorting_params

--- lib/whammy/command_line_options_parser.rb
+++ lib/whammy/command_line_options_parser.rb
@@ -1,8 +1,17 @@
 module Whammy
   class CommandLineOptionsParser
-    
+
     def initialize
-      @options_parser = OptionParser.new
+      @sorting_params = {}
+      @options_parser = OptionParser.new do |opts|
+        opts.on("--sort ENUM", ["-b", "-g", "-l"]) do |flag|
+          case flag
+          when "-b" then @sorting_params[:sort_by] = :birthday
+          when "-g" then @sorting_params[:sort_by] = :gender
+          when "-l" then @sorting_params[:sort_by] = :lastname
+          end
+        end
+      end
     end
 
   end
