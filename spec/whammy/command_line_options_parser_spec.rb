3375c12a80d95a20cc883752a579bd9925aecc6c
Tam T Dang <teedang19@gmail.com>
2015-03-27 12:23:07 -0500

    empty command_line_options_parser spec

---
+++ spec/whammy/command_line_options_parser_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../spec_helper"
+
+module Whammy
+  describe CommandLineOptionsParser do
+    let(:argv) { ["commas.txt", "-b"] }
+    let(:parser) { CommandLineOptionsParser.new }
+
+    describe "#initialize" do
+    end
+  end
+end
