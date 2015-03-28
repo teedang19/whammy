192fd49a13a4b360c1deee799c478f54e183c1e2
Tam T Dang <teedang19@gmail.com>
2015-03-28 10:12:57 -0500

    restructure fo command_line_options_parser to remove the split of setting files and sorting options

--- lib/whammy/command_line_options_parser.rb
+++ lib/whammy/command_line_options_parser.rb
@@ -1,28 +1,31 @@
 module Whammy
   class CommandLineOptionsParser
-    def initialize
-      @sorting_params = {}
-      @options_parser = OptionParser.new do |opts|
-        opts.on("--sort ENUM", ["-b", "-g", "-l"]) do |flag|
-          case flag
-          when "-b" then @sorting_params[:sort_by] = :birthday
-          when "-g" then @sorting_params[:sort_by] = :gender # TODO sort by gender is also last_name ascending. where does this logic go?
-          when "-l" then @sorting_params[:sort_by] = :last_name
-          end
-        end
-      end
+    attr_reader :files, :sorting_params
+
+    def initialize(argv)
+      @options_parser = OptionParser.new
+      @argv = argv
+      @files, @sorting_params = parse_options!
     end
 
-    def get_files(argv)
-      @options_parser.parse(argv)
+    def parse_options!
+      sorting_params = {}
+
+      @options_parser.on("--sort ENUM", ["-b", "-g", "-l"]) do |flag|
+        case flag
+        when "-b" then sorting_params[:sort_by] = :birthday
+        when "-g" then sorting_params[:sort_by] = :gender # TODO sort by gender is also last_name ascending. where does this logic go?
+        when "-l" then sorting_params[:sort_by] = :last_name
+        end
+      end
+      
+      files = @options_parser.parse(@argv)
+      [files, sorting_params]
+
       rescue OptionParser::InvalidOption => e # TODO can we combine this error handling?
         puts "#{e.message}\nTry again!"
       rescue OptionParser::InvalidArgument => e
         puts "#{e.message}\nTry again!"
-    end
-
-    def sorting_params
-      @sorting_params
     end
   end
 end
