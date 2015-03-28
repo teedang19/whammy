7220eb78df9f95e8746bd9a43b47281a4dd28131
Tam T Dang <teedang19@gmail.com>
2015-03-28 11:46:01 -0500

    delete command_line_interface #parse_options method, now have two methods that delegate to @option_parser

--- lib/whammy/command_line_interface.rb
+++ lib/whammy/command_line_interface.rb
@@ -10,10 +10,6 @@ module Whammy
     def sort # TODO will call a Sort class ?
     end
 
-    def parse_options(argv)
-      # use OptionParser to separate argv into filenames and flags/options to pass to a Sorter class
-    end
-
     def sorting_params
       @options_parser.sorting_params
     end
