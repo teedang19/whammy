185fbf687d9f9c1302ff4455f9134f73f58e0408
Tam T Dang <teedang19@gmail.com>
2015-03-28 11:16:23 -0500

    update to command_line_options_parser_spec to fit new structure of CommandLineOptionsParser

--- spec/whammy/command_line_options_parser_spec.rb
+++ spec/whammy/command_line_options_parser_spec.rb
@@ -3,7 +3,7 @@ require_relative "../spec_helper"
 module Whammy
   describe CommandLineOptionsParser do
     let(:argv) { ["commas.txt", "--sort", "-b"] }
-    let(:parser) { CommandLineOptionsParser.new }
+    let(:parser) { CommandLineOptionsParser.new(argv) }
 
     describe "#initialize" do
       it "defines @options_parser" do
@@ -16,54 +16,48 @@ module Whammy
 
       it "defines @sorting_params" do
         expect(parser.instance_variable_get(:@sorting_params)).to_not be_nil
-      end
-
-      it "establishes @sorting_params as a Hash" do
         expect(parser.instance_variable_get(:@sorting_params)).to be_a(Hash)
       end
-    end
 
-    describe "#get_files" do
-      context "when called with one file" do
-        it "returns an array of the file" do
-          expect(parser.get_files(argv)).to eql(["commas.txt"])
-        end
-      end
-
-      context "when called with multiple files" do
-        it "returns an array of all the files" do
-          two_file_argv = ["commas.txt", "pipes.txt", "--sort", "-g"]
-          expect(parser.get_files(two_file_argv)).to eql(["commas.txt", "pipes.txt"])
-        end
+      it "defines @files" do
+        expect(parser.instance_variable_get(:@files)).to_not be_nil
+        expect(parser.instance_variable_get(:@files)).to be_a(Array)
       end
     end
 
-    describe "#sorting_params" do
-      context "when called with params" do
-        it "returns the correct params for birthday" do
-          parser.instance_variable_get(:@options_parser).parse(argv) # must be called to pass in the ARGV
-          expect(parser.sorting_params).to eql({ sort_by: :birthday })
-        end
-
-        it "returns the correct params for gender" do
-          gender_argv = ["commas.txt", "--sort", "-g"]
-          parser.instance_variable_get(:@options_parser).parse(gender_argv)
-          expect(parser.sorting_params).to eql({ sort_by: :gender })
-        end
-
-        it "returns the correct params for last name" do
-          last_name_argv = ["commas.txt", "--sort", "-l"]
-          parser.instance_variable_get(:@options_parser).parse(last_name_argv)
-          expect(parser.sorting_params).to eql({ sort_by: :last_name })
-        end
+    describe "#parse_options!" do
+      it "returns an array" do
+        expect(parser.parse_options!).to be_a(Array)
       end
 
-      context "when called without sorting params" do
-        it "returns an empty hash" do
-          no_sort_argv = ["commas.txt", "pipes.txt"]
-          parser.instance_variable_get(:@options_parser).parse(no_sort_argv)
-          expect(parser.sorting_params).to eql( {} )
-        end
+      it "separates the files from the passed-in options" do
+        expect(parser.parse_options![0]).to eql(["commas.txt"])
+      end
+
+      it "can separate more than one file" do
+        multiple_file_argv = ["commas.txt", "pipes.txt", "--sort", "-g"]
+        multiple_file_parser = CommandLineOptionsParser.new(multiple_file_argv)
+        expect(multiple_file_parser.parse_options![0]).to eql(["commas.txt", "pipes.txt"])
+      end
+
+      it "separates the sorting params from the passed-in options" do
+        expect(parser.parse_options![1]).to have_key(:sort_by)
+      end
+
+      it "returns the correct sorting params for birthday" do
+        expect(parser.parse_options![1]).to eql({ sort_by: :birthday })
+      end
+
+      it "returns the correct sorting params for gender" do
+        gender_argv = ["commas.txt", "--sort", "-g"]
+        gender_parser = CommandLineOptionsParser.new(gender_argv)
+        expect(gender_parser.parse_options![1]).to eql({ sort_by: :gender })
+      end
+
+      it "returns the correct sorting params for birthday" do
+        last_name_argv = ["commas.txt", "--sort", "-l"]
+        last_name_parser = CommandLineOptionsParser.new(last_name_argv)
+        expect(last_name_parser.parse_options![1]).to eql({ sort_by: :last_name })
       end
     end
   end
