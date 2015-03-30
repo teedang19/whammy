dc208849b032990fd9c8ef4111f1d58b7680d753
Tam T Dang <teedang19@gmail.com>
2015-03-29 23:57:18 -0500

    parser #parse method passes each file to #parse_file, specs to test

--- lib/whammy/parser.rb
+++ lib/whammy/parser.rb
@@ -1,7 +1,15 @@
 module Whammy
   class Parser
-    def initialize(files_array)
+
+    def initialize(files_array) # TODO should a Parser only be initialized with one file?
       @files = files_array
+    end
+
+    def parse! # TODO should a Parser only be initialized with one file?
+      @files.map { |file| parse_file(file) }
+    end
+
+    def parse_file(file)
     end
   end
 end
--- spec/whammy/parser_spec.rb
+++ spec/whammy/parser_spec.rb
@@ -10,5 +10,19 @@ module Whammy
         expect(parser.instance_variable_get(:@files)).to_not be_nil
       end
     end
+
+    describe "#parse!" do
+      it "passes each file to #parse_file" do
+        expect(parser).to receive(:parse_file).with(files[0])
+        expect(parser).to receive(:parse_file).with(files[1])
+        parser.parse!
+      end
+
+      xit "returns the files parsed" do
+      end
+    end
+
+    describe "#parse_file(file)" do
+    end
   end
 end
