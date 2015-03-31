aada67472fe0ab8a965b3034e395405f83372a30
Tam T Dang <teedang19@gmail.com>
2015-03-31 08:08:44 -0500

    Database.rb requires date, sets constant for master db, and #initialize sets filename

--- lib/whammy/database.rb
+++ lib/whammy/database.rb
@@ -1,4 +1,11 @@
+require "date"
+
 module Whammy
   class Database
+    MASTER_DB = "database.txt"
+
+    def initialize(write_to_master)
+      @filename = write_to_master ? MASTER_DB : DateTime.now.strftime("%m_%e_%y:%k_%M.txt")
+    end
   end
 end
