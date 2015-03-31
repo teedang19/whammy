459db98b3304ec5c907025eaa5d114aecfdd5c39
Tam T Dang <teedang19@gmail.com>
2015-03-31 07:53:47 -0500

    uncomment out methods, will be handled later

--- lib/whammy/parser.rb
+++ lib/whammy/parser.rb
@@ -1,28 +1,28 @@
 module Whammy
   class Parser
     DELIMITERS = [ /, /, / \| /, / / ]
-    # ATTRIBUTES = [ :last_name, :first_name, :gender, :favorite_color, :date_of_birth ]
+    ATTRIBUTES = [ :last_name, :first_name, :gender, :favorite_color, :date_of_birth ]
 
     def initialize(files_array) # TODO should a Parser only be initialized with one file?
       @files = files_array
     end
 
-    # def parsed_data
-    #   parsed_files.flatten
-    # end
+    def parsed_data
+      parsed_files.flatten
+    end
 
-    # def parsed_files # TODO should a Parser only be initialized with one file?
-    #   @files.map { |file| parse_file!(file) }
-    # end
+    def parsed_files # TODO should a Parser only be initialized with one file?
+      @files.map { |file| parse_file!(file) }
+    end
 
-    # def parse_file!(file)
-    #   File.readlines(file).map { |line| parse_line!(line) }
-    # end
+    def parse_file!(file)
+      File.readlines(file).map { |line| parse_line!(line) }
+    end
 
-    # def parse_line!(line)
-    #   values_arr = split_line!(line)
-    #   attributeify(values_arr)
-    # end
+    def parse_line!(line)
+      values_arr = split_line!(line)
+      attributeify(values_arr)
+    end
 
     def split_line!(line)
       line.chomp.split(delimiter(line))
@@ -32,10 +32,10 @@ module Whammy
       DELIMITERS.detect { |delimiter| line =~ delimiter }
     end
 
-    # def attributeify(values_arr)
-    #   raise ArgumentError.new("There are records in your file with an incorrect number of attributes.") unless values_arr.count == ATTRIBUTES.count
+    def attributeify(values_arr)
+      raise ArgumentError.new("There are records in your file with an incorrect number of attributes.") unless values_arr.count == ATTRIBUTES.count
 
-    #   ATTRIBUTES.each_with_index.map { |attribute, index| [attribute, values_arr[index]] }.to_h
-    # end
+      ATTRIBUTES.each_with_index.map { |attribute, index| [attribute, values_arr[index]] }.to_h
+    end
   end
 end
