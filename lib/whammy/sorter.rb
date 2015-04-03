052df61d7b94b6b0a471ad2a8860b486af894007
Tam T Dang <teedang19@gmail.com>
2015-04-03 14:14:25 -0500

    remove last_name_ascending from sorter since it isn't being used, and the testing of private methods from sorter spec

--- lib/whammy/sorter.rb
+++ lib/whammy/sorter.rb
@@ -25,21 +25,8 @@ module Whammy
       @data.sort_by { |record| Date.strptime(record[:date_of_birth], '%m/%d/%Y') }
     end
 
-    def last_name_ascending # TODO this is untested
-      last_name_sort(:asc)
-    end
-
     def last_name_descending
-      last_name_sort(:desc)
-    end
-
-    def last_name_sort(direction)
-      case direction
-      when :asc
-        @data.sort { |record1, record2| record1[:last_name] <=> record2[:last_name] }
-      when :desc
-        @data.sort { |record1, record2| record2[:last_name] <=> record1[:last_name] }
-      end
+      @data.sort { |record1, record2| record2[:last_name] <=> record1[:last_name] }
     end
   end
 end
--- spec/whammy/sorter_spec.rb
+++ spec/whammy/sorter_spec.rb
@@ -43,27 +43,5 @@ module Whammy
         end
       end
     end
-
-    describe "#last_name_ascending" do
-      xit "calls #last_name_sort with correct parameters" do
-        expect(sorter).to receive(:last_name_sort).with(:asc)
-        sorter.last_name_ascending
-      end
-
-      xit "sorts @data by last_name, ascending" do
-        expect(sorter.last_name_ascending).to eql([{last_name: "Dang", first_name: "Tam", gender: "female", favorite_color: "blue", date_of_birth: "01/13/1990"}, {last_name: "Govan", first_name: "Guthrie", gender: "male", favorite_color: "blue", date_of_birth: "12/27/1971"}, {last_name: "Reinhardt", first_name: "Django", gender: "male", favorite_color: "green", date_of_birth: "01/23/1910"}, {last_name: "Schemel", first_name: "Patty", gender: "female", favorite_color: "orange", date_of_birth: "04/24/1967"}, {last_name: "Schuldiner", first_name: "Chuck", gender: "male", favorite_color: "orange", date_of_birth: "05/13/1967"}])
-      end
-    end
-
-    describe "#last_name_descending" do
-      xit "calls #last_name_sort with correct parameters" do
-        expect(sorter).to receive(:last_name_sort).with(:desc)
-        sorter.last_name_descending
-      end
-
-      xit "sorts @data by last_name, descending" do
-        expect(sorter.last_name_descending).to eql([{last_name: "Schuldiner", first_name: "Chuck", gender: "male", favorite_color: "orange", date_of_birth: "05/13/1967"}, {last_name: "Schemel", first_name: "Patty", gender: "female", favorite_color: "orange", date_of_birth: "04/24/1967"}, {last_name: "Reinhardt", first_name: "Django", gender: "male", favorite_color: "green", date_of_birth: "01/23/1910"}, {last_name: "Govan", first_name: "Guthrie", gender: "male", favorite_color: "blue", date_of_birth: "12/27/1971"}, {last_name: "Dang", first_name: "Tam", gender: "female", favorite_color: "blue", date_of_birth: "01/13/1990"}])
-      end
-    end
   end
 end
