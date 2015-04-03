e603d538db02bfb54bfa622ce0d8bcd71330ead1
Tam T Dang <teedang19@gmail.com>
2015-04-03 10:51:23 -0500

    sorter spec: allow db read from to be data/example_db.txt and data dir with other files

--- spec/whammy/sorter_spec.rb
+++ spec/whammy/sorter_spec.rb
@@ -4,9 +4,17 @@ module Whammy
   describe Sorter do
     let(:sorter) { Sorter.new }
 
+    before(:each) do
+      allow_any_instance_of(Database).to receive(:data_file).and_return("data/example_db.txt")
+    end
+
     describe "#initialize" do
       it "defines @data" do
         expect(sorter.instance_variable_get(:@data)).to_not be_nil
+      end
+
+      it "sets @data to the data read from the database" do
+        expect(sorter.instance_variable_get(:@data)).to eql([{last_name: "Govan", first_name: "Guthrie", gender: "male", favorite_color: "blue", date_of_birth: "12/27/1971"},{last_name: "Schuldiner", first_name: "Chuck", gender: "male", favorite_color: "orange", date_of_birth: "05/13/1967"},{last_name: "Reinhardt", first_name: "Django", gender: "male", favorite_color: "green", date_of_birth: "01/23/1910"}])
       end
     end
   end
