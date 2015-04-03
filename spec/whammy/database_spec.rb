7d08cf9e9afcf36d4e7ceecd807e8a04ab58472f
Tam T Dang <teedang19@gmail.com>
2015-04-02 22:16:51 -0500

    update to database_spec to fit new Database model without #write_to_master options

--- spec/whammy/database_spec.rb
+++ spec/whammy/database_spec.rb
@@ -4,77 +4,37 @@ require_relative "../spec_helper"
 
 module Whammy
   describe Database do
-    let(:master_db) { Database.new(true) }
-    let(:temp_db) { Database.new(false) }
+    let(:db) { Database.new }
     let(:line_data) { ["Govan Guthrie male blue 12/27/1971", "Schuldiner Chuck male orange 05/13/1967"] }
 
     describe "#initialize" do
-      context "when writing to master" do
-        it "defines @filename" do
-          expect(master_db.instance_variable_get(:@filename)).to_not be_nil
-        end
-
-        it "sets @filename to the master db" do
-          expect(master_db.instance_variable_get(:@filename)).to eql("database.txt")
-        end
+      it "defines @filename" do
+        expect(db.instance_variable_get(:@filename)).to_not be_nil
       end
 
-      context "when writing to a new file" do
-        it "defines @filename" do
-          expect(temp_db.instance_variable_get(:@filename)).to_not be_nil
-        end
-
-        it "sets @filename to a Timestamp string" do
-          expect(temp_db.instance_variable_get(:@filename)).to eql(DateTime.now.strftime("%m_%e_%y:%k_%M.txt"))
-        end
+      it "sets @filename to the master db" do
+        expect(db.instance_variable_get(:@filename)).to eql("database.txt")
       end
     end
 
     describe "#data_dir" do
-      context "when writing to master" do
-        it "returns the path of the data directory" do
-          expect(master_db.data_dir).to eql("data/")
-        end
-      end
-
-      context "when writing to a new file" do
-        it "returns the path of the data directory" do
-          expect(temp_db.data_dir).to eql("data/")
-        end
+      it "returns the path of the data directory" do
+        expect(db.data_dir).to eql("data/")
       end
     end
 
     describe "#filename" do
-      context "when writing to master" do
-        it "returns the path of the data file" do
-          filename = master_db.instance_variable_get(:@filename)
-          expect(master_db.filename).to eql("data/#{filename}")
-        end
-      end
-
-      context "when writing to a new file" do
-        it "returns the path of the data file" do
-          filename = temp_db.instance_variable_get(:@filename)
-          expect(temp_db.filename).to eql("data/#{filename}")
-        end
+      it "returns the path of the data file" do
+        filename = db.instance_variable_get(:@filename)
+        expect(db.filename).to eql("data/#{filename}")
       end
     end
 
     describe "#write!" do
-      context "when writing to master" do
-        it "writes the data to the file" do
-          filename = master_db.instance_variable_get(:@filename)
-          master_db.write!(line_data)
-          expect(File.read(master_db.filename)).to include("Govan Guthrie male blue 12/27/1971\nSchuldiner Chuck male orange 05/13/1967\n")
-        end
-      end
-
-      context "when writing to a new file" do
-        it "writes the data to the file" do
-          filename = temp_db.instance_variable_get(:@filename)
-          temp_db.write!(line_data)
-          expect(File.read(temp_db.filename)).to include("Govan Guthrie male blue 12/27/1971\nSchuldiner Chuck male orange 05/13/1967\n")
-        end
+      it "writes the data to the file" do
+        filename = db.instance_variable_get(:@filename)
+        db.write!(line_data)
+        expect(File.read(db.filename)).to include("Govan Guthrie male blue 12/27/1971\nSchuldiner Chuck male orange 05/13/1967\n")
       end
     end
   end
