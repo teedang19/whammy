6e434682ce2b6489872c1ce543af2f671755d980
Tam T Dang <teedang19@gmail.com>
2015-04-03 10:59:14 -0500

    ladies_first sorter method and spec

--- lib/whammy/sorter.rb
+++ lib/whammy/sorter.rb
@@ -3,5 +3,9 @@ module Whammy
     def initialize
       @data = Database.new.read # will be master
     end
+
+    def ladies_first
+      @data.sort_by { |record| record[:gender] }
+    end
   end
 end
--- spec/whammy/sorter_spec.rb
+++ spec/whammy/sorter_spec.rb
@@ -17,5 +17,11 @@ module Whammy
         expect(sorter.instance_variable_get(:@data)).to eql([{last_name: "Govan", first_name: "Guthrie", gender: "male", favorite_color: "blue", date_of_birth: "12/27/1971"},{last_name: "Schuldiner", first_name: "Chuck", gender: "male", favorite_color: "orange", date_of_birth: "05/13/1967"},{last_name: "Reinhardt", first_name: "Django", gender: "male", favorite_color: "green", date_of_birth: "01/23/1910"}])
       end
     end
+
+    describe "#ladies_first" do
+      it "sorts @data by gender, female first" do
+        expect(sorter.ladies_first).to eql([{last_name: "Dang", first_name: "Tam", gender: "female", favorite_color: "blue", date_of_birth: "01/13/1990"}, {last_name: "Schemel", first_name: "Patty", gender: "female", favorite_color: "orange", date_of_birth: "04/24/1967"},{last_name: "Schuldiner", first_name: "Chuck", gender: "male", favorite_color: "orange", date_of_birth: "05/13/1967"}, {last_name: "Reinhardt", first_name: "Django", gender: "male", favorite_color: "green", date_of_birth: "01/23/1910"},{last_name: "Govan", first_name: "Guthrie", gender: "male", favorite_color: "blue", date_of_birth: "12/27/1971"}])
+      end
+    end
   end
 end
