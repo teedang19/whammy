7ab69f49b4783f9bc74e756c2ad5f1ada209309e
Tam T Dang <teedang19@gmail.com>
2015-04-03 11:04:11 -0500

    sorter #oldest_first method and spec

--- lib/whammy/sorter.rb
+++ lib/whammy/sorter.rb
@@ -7,5 +7,9 @@ module Whammy
     def ladies_first
       @data.sort_by { |record| record[:gender] }
     end
+
+    def oldest_first
+      @data.sort_by { |record| Date.strptime(record[:date_of_birth], '%m/%d/%Y') }
+    end
   end
 end
