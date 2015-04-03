e64725c72132ddc6a6749c381b2fdee4d430956a
Tam T Dang <teedang19@gmail.com>
2015-04-03 15:17:25 -0500

    namespace API under v1 dir and module

--- lib/whammy/v1/api.rb
+++ lib/whammy/v1/api.rb
@@ -1,7 +1,11 @@
 module Whammy
-  class V1::Api < Grape::API
-    version 'v1', using: :header, vendor: 'whammy'
-    format :json
-    prefix 'api/v1'
+  module V1
+    require 'grape'
+
+    class API < Grape::API
+      version 'v1', using: :header, vendor: 'whammy'
+      format :json
+      prefix 'api/v1'
+    end
   end
 end
