8a35eeda8be618d0ed063d3ce6b53a9782735269
Tam T Dang <teedang19@gmail.com>
2015-04-04 10:46:38 -0500

    basic skeleton for testing API per grape documentation

--- spec/whammy/v1/api_spec.rb
+++ spec/whammy/v1/api_spec.rb
@@ -1,7 +1,14 @@
 require_relative "../../spec_helper"
+require "rack/test"
+
 module Whammy
   module V1
     describe API do
+      include Rack::Test::Methods
+
+      def app
+        Whammy::V1::API
+      end
     end
   end
 end
