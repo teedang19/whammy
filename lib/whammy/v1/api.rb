f015d601158618a011f276f770bfc2d27f87fef8
Tam T Dang <teedang19@gmail.com>
2015-04-04 11:35:25 -0500

    specs for sort routes

--- lib/whammy/v1/api.rb
+++ lib/whammy/v1/api.rb
@@ -14,14 +14,17 @@ module Whammy
 
         desc "return records sorted by gender, women first && last name ascending"
         get :gender do
+          { records: Sorter.new.sort!(:gender) }
         end
 
         desc "return records sorted by birthdate, oldest first"
         get :birthdate do
+          { records: Sorter.new.sort!(:birthdate) }
         end
 
         desc "return records sorted by last name, descending"
         get :name do
+          { records: Sorter.new.sort!(:last_name) }
         end
       end
     end
--- spec/whammy/v1/api_spec.rb
+++ spec/whammy/v1/api_spec.rb
@@ -37,31 +37,37 @@ module Whammy
       end
 
       describe "GET /api/v1/records/gender" do
-        xit "returns database records sorted by gender" do
-        end
+        before(:each) { get "api/v1/records/gender" }
 
         it "returns a 200 status" do
-          get "api/v1/records/gender"
           expect(last_response.status).to eq(200)
+        end
+
+        it "returns database records sorted by gender: women first && last name asc" do
+          expect(JSON.parse(last_response.body)).to eq({"records"=>[{"last_name"=>"Dang", "first_name"=>"Tam", "gender"=>"female", "favorite_color"=>"blue", "date_of_birth"=>"01/13/1990"}, {"last_name"=>"Schemel", "first_name"=>"Patty", "gender"=>"female", "favorite_color"=>"orange", "date_of_birth"=>"04/24/1967"}, {"last_name"=>"Govan", "first_name"=>"Guthrie", "gender"=>"male", "favorite_color"=>"blue", "date_of_birth"=>"12/27/1971"}, {"last_name"=>"Reinhardt", "first_name"=>"Django", "gender"=>"male", "favorite_color"=>"green", "date_of_birth"=>"01/23/1910"}, {"last_name"=>"Schuldiner", "first_name"=>"Chuck", "gender"=>"male", "favorite_color"=>"orange", "date_of_birth"=>"05/13/1967"}]})
         end
       end
 
       describe "GET /api/v1/records/birthdate" do
-        xit "returns database records sorted by birthdate" do
+        before(:each) { get "api/v1/records/birthdate" }
+
+        it "returns database records sorted birthdate, asc" do
+          expect(JSON.parse(last_response.body)).to eq({"records"=>[{"last_name"=>"Reinhardt", "first_name"=>"Django", "gender"=>"male", "favorite_color"=>"green", "date_of_birth"=>"01/23/1910"}, {"last_name"=>"Schemel", "first_name"=>"Patty", "gender"=>"female", "favorite_color"=>"orange", "date_of_birth"=>"04/24/1967"}, {"last_name"=>"Schuldiner", "first_name"=>"Chuck", "gender"=>"male", "favorite_color"=>"orange", "date_of_birth"=>"05/13/1967"}, {"last_name"=>"Govan", "first_name"=>"Guthrie", "gender"=>"male", "favorite_color"=>"blue", "date_of_birth"=>"12/27/1971"}, {"last_name"=>"Dang", "first_name"=>"Tam", "gender"=>"female", "favorite_color"=>"blue", "date_of_birth"=>"01/13/1990"}]})
         end
 
         it "returns a 200 status" do
-          get "api/v1/records/birthdate"
           expect(last_response.status).to eq(200)
         end
       end
 
       describe "GET /api/v1/records/name" do
-        xit "returns database records sorted by last_name" do
+        before(:each) { get "api/v1/records/name" }
+
+        it "returns database records sorted last_name desc" do
+          expect(JSON.parse(last_response.body)).to eq({"records"=>[{"last_name"=>"Schuldiner", "first_name"=>"Chuck", "gender"=>"male", "favorite_color"=>"orange", "date_of_birth"=>"05/13/1967"}, {"last_name"=>"Schemel", "first_name"=>"Patty", "gender"=>"female", "favorite_color"=>"orange", "date_of_birth"=>"04/24/1967"}, {"last_name"=>"Reinhardt", "first_name"=>"Django", "gender"=>"male", "favorite_color"=>"green", "date_of_birth"=>"01/23/1910"}, {"last_name"=>"Govan", "first_name"=>"Guthrie", "gender"=>"male", "favorite_color"=>"blue", "date_of_birth"=>"12/27/1971"}, {"last_name"=>"Dang", "first_name"=>"Tam", "gender"=>"female", "favorite_color"=>"blue", "date_of_birth"=>"01/13/1990"}]})
         end
 
         it "returns a 200 status" do
-          get "api/v1/records/name"
           expect(last_response.status).to eq(200)
         end
       end
