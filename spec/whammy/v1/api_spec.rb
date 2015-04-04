cb1a36605b3a78c346f10a8d5b3c1f38b6f435b5
Tam T Dang <teedang19@gmail.com>
2015-04-04 11:03:27 -0500

    skeleton specs for whammy api routes: post, get /gender, /name, and /birthdate

--- spec/whammy/v1/api_spec.rb
+++ spec/whammy/v1/api_spec.rb
@@ -9,6 +9,44 @@ module Whammy
       def app
         Whammy::V1::API
       end
+
+      describe "POST /api/v1/records" do
+        xit "posts a JSON record" do
+        end
+
+        xit "doesn't allow records to post without proper parameters" do
+        end
+
+        xit "returns the posted record" do
+        end
+
+        xit "returns a 301 for posted records" do
+        end
+      end
+
+      describe "GET /api/v1/records/gender" do
+        xit "returns database records sorted by gender" do
+        end
+
+        xit "returns a 200" do
+        end
+      end
+
+      describe "GET /api/v1/records/birthdate" do
+        xit "returns database records sorted by birthdate" do
+        end
+
+        xit "returns a 200" do
+        end
+      end
+
+      describe "GET /api/v1/records/name" do
+        xit "returns database records sorted by last_name" do
+        end
+
+        xit "returns a 200" do
+        end
+      end
     end
   end
 end
