294f1d145d09989c36e67a0d0aee39e0ce56e2f3
Tam T Dang <teedang19@gmail.com>
2015-04-05 18:57:17 -0500

    picky: whammy.rb requires api file first

--- lib/whammy.rb
+++ lib/whammy.rb
@@ -1,8 +1,8 @@
 require "optparse"
 
+require_relative "whammy/v1/api"
 require_relative "whammy/command_line_interface"
 require_relative "whammy/command_line_options_parser"
 require_relative "whammy/database"
 require_relative "whammy/parser"
 require_relative "whammy/sorter"
-require_relative "whammy/v1/api"
