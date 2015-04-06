d59b5917c18bd0f2b7d44ea20dd59bb65afa66f0
Tam T Dang <teedang19@gmail.com>
2015-04-06 08:16:47 -0500

    pick about spacing in default values for params in database.rb

--- lib/whammy/database.rb
+++ lib/whammy/database.rb
@@ -4,7 +4,7 @@ module Whammy
   class Database
     MASTER_DB = "database.txt"
 
-    def initialize(use_master=true)
+    def initialize(use_master = true)
       @filename = use_master ? MASTER_DB : DateTime.now.strftime("%m_%d_%y:%k_%M.txt")
       @parser = Parser.new
     end
