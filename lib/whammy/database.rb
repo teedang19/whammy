e0f683aed7c6a15b051dc9211be92ebdc70cea2d
Tam T Dang <teedang19@gmail.com>
2015-04-06 08:12:05 -0500

    database implements write to master feature, specs

--- lib/whammy/database.rb
+++ lib/whammy/database.rb
@@ -4,8 +4,8 @@ module Whammy
   class Database
     MASTER_DB = "database.txt"
 
-    def initialize(master=true)
-      @filename = master ? MASTER_DB : DateTime.now.strftime("%m_%d_%y:%k_%M.txt")
+    def initialize(use_master=true)
+      @filename = use_master ? MASTER_DB : DateTime.now.strftime("%m_%d_%y:%k_%M.txt")
       @parser = Parser.new
     end
 
