c8c46c3ebbc16c05d9452f3ad3ce8b5614f03896
Tam T Dang <teedang19@gmail.com>
2015-04-09 10:39:10 -0500

    spec helper overwrites stdout to avoid terminal output during tests. doesn't work for options parser rescue error, only have suspicions as to why. works for command line interface

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ SimpleCov.start
 
 require_relative "../lib/whammy"
 require "rspec"
+
+RSpec.configure do |config|
+  config.before do
+    $stdout = StringIO.new
+  end
+  config.after(:all) do
+    $stdout = STDOUT
+  end
+end
