73d50b329653fb3ac33742d42c78ed0231aabc4e
Tam T Dang <teedang19@gmail.com>
2015-04-08 23:28:34 -0500

    remove double test from merging cleanup branch

--- spec/whammy/sorter_spec.rb
+++ spec/whammy/sorter_spec.rb
@@ -48,13 +48,6 @@ module Whammy
         expect(random_db).to receive(:read).exactly(1).times
         Sorter.new(random_db)
       end
-
-      let(:random_db) { Database.new }
-
-      it "reads from the database passed in, if there is one" do
-        expect(random_db).to receive(:read).exactly(1).times
-        Sorter.new(random_db)
-      end
     end
 
     describe "#sort!" do
