8816ca5bac41779a9a67b55a5b116925e0293484
Tam T Dang <teedang19@gmail.com>
2015-04-12 21:32:48 -0500

    updates to sorter spec initialize with contexts about a db passed in or using the master

--- spec/whammy/sorter_spec.rb
+++ spec/whammy/sorter_spec.rb
@@ -9,38 +9,19 @@ module Whammy
     end
 
     describe "#initialize" do
-      it "sets @data to the data read from the database" do
-        expect(sorter.instance_variable_get(:@data)).to eql(
-          [
-            {
-              last_name: "Schemel",
-              first_name: "Patty",
-              gender: "female",
-              favorite_color: "orange",
-              date_of_birth: "04/24/1967"
-            },
-            {
-              last_name: "Schuldiner",
-              first_name: "Chuck",
-              gender: "male",
-              favorite_color: "orange",
-              date_of_birth: "05/13/1967"
-            },
-            {
-              last_name: "Reinhardt",
-              first_name: "Django",
-              gender: "male",
-              favorite_color: "green",
-              date_of_birth: "01/23/1910"
-            }
-          ]
-        )
+      context "with a database" do
+        it "reads from the passed-in database" do
+          random_db = Database.new
+          expect(random_db).to receive(:read).exactly(1).times
+          Sorter.new(random_db)
+        end
       end
 
-      it "reads from the database passed in, if there is one" do
-        random_db = Database.new
-        expect(random_db).to receive(:read).exactly(1).times
-        Sorter.new(random_db)
+      context "without a database" do
+        it "reads from the master database" do
+          expect(Database).to receive(:master_read).exactly(1).times
+          Sorter.new
+        end
       end
     end
 
