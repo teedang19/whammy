7bed135ec22567b0d421dd4ef667bf787af3c514
Tam T Dang <teedang19@gmail.com>
2015-04-13 22:20:17 -0500

    remove instance vars from sorter.rb

--- lib/whammy/sorter.rb
+++ lib/whammy/sorter.rb
@@ -2,6 +2,11 @@ require "date"
 
 module Whammy
   class Sorter
+    private
+    attr_reader :data
+
+    public
+
     def initialize(database = nil)
       @data = database ? database.read : Database.new.read # will be master
     end
@@ -11,22 +16,22 @@ module Whammy
       when :gender then ladies_first
       when :birthdate then oldest_first
       when :last_name then last_name_descending
-      else @data
+      else data
       end
     end
 
     private
 
     def ladies_first
-      @data.sort_by { |record| [record[:gender], record[:last_name]] }
+      data.sort_by { |record| [record[:gender], record[:last_name]] }
     end
 
     def oldest_first
-      @data.sort_by { |record| Date.strptime(record[:date_of_birth], '%m/%d/%Y') }
+      data.sort_by { |record| Date.strptime(record[:date_of_birth], '%m/%d/%Y') }
     end
 
     def last_name_descending
-      @data.sort { |record1, record2| record2[:last_name] <=> record1[:last_name] }
+      data.sort { |record1, record2| record2[:last_name] <=> record1[:last_name] }
     end
   end
 end
